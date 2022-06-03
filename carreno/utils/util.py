17f6492ac76208e25365fbfd1473fe6052b13cda
lemphi97 <lemphi97@gmail.com>
2022-06-03 14:23:01 -0400

    patchify with 2d example

--- carreno/utils/util.py
+++ carreno/utils/util.py
@@ -53,7 +53,7 @@ def unstack(a, axis=0):
     :param axis: axis to unstack
     :return: unstacked array
     """
-    return [np.squeeze(e, axis) for e in np.split(a, a.shape[axis], axis = axis)]
+    return [np.squeeze(e, axis) for e in np.split(a, a.shape[axis], axis=axis)]
     
 
 def coord2np(coord):
@@ -64,3 +64,33 @@ def coord2np(coord):
     :return:      numpy index (list)
     """
     return tuple([[i] for i in coord])
+
+
+def is_2D(shape):
+    """check if shape is probably a 2D image
+    Parameters
+    ----------
+    shape : [int]
+        ndarray shape
+    Returns
+    -------
+    _ : bool
+        if shape could be for 2D image
+    """
+    # check if grayscale, rgb or rgba
+    return len(shape) == 2 or (len(shape) == 3 and (shape[2] == 3 or shape[2] == 4))
+
+
+def is_3D(shape):
+    """check if shape is probably a 3D volume
+    Parameters
+    ----------
+    shape : [int]
+        ndarray shape
+    Returns
+    -------
+    _ : bool
+        if shape could be for 3D volume
+    """
+    # check if grayscale, rgb or rgba
+    return len(shape) == 3 or (len(shape) == 4 and (shape[3] == 3 or shape[3] == 4))
