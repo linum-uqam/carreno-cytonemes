2a18ec91b5de8f9b21a956ed3177d928bd3fabc9
lemphi97 <lemphi97@gmail.com>
2023-04-05 17:58:23 -0400

    soft skeleton experiment

--- carreno/nn/soft_skeleton.py
+++ carreno/nn/soft_skeleton.py
@@ -2,10 +2,11 @@
 # Code adapted from https://github.com/jocpae/clDice/blob/master/cldice_loss/keras/soft_skeleton.py
 #
 
-from keras import layers as KL
-from keras import backend as K
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import layers as KL
+from tensorflow.keras import backend as K
+
 
 def soft_erode2D(img):
     """[This function performs soft-erosion operation on a float32 image]
@@ -14,7 +15,7 @@ def soft_erode2D(img):
     Returns:
         [float32]: [the eroded image]
     """
-    return -KL.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding='same', data_format=None)(-img)
+    return -KL.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding='same')(-img)
 
 
 def soft_erode3D(img):
@@ -25,7 +26,7 @@ def soft_erode3D(img):
         [float32]: [the eroded image]
     """
     # changed to 3x3x3 to be compatible with dilation
-    return -KL.MaxPool3D(pool_size=(3, 3, 3), strides=(1, 1, 1), padding='same', data_format=None)(-img)
+    return -KL.MaxPool3D(pool_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(-img)
 
 
 def soft_dilate2D(img):
@@ -35,7 +36,7 @@ def soft_dilate2D(img):
     Returns:
         [float32]: [the dialated image]
     """
-    return KL.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding='same', data_format=None)(img)
+    return KL.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding='same')(img)
 
 
 def soft_dilate3D(img):
@@ -45,7 +46,7 @@ def soft_dilate3D(img):
     Returns:
         [float32]: [the dialated image]
     """
-    return KL.MaxPool3D(pool_size=(3, 3, 3), strides=(1, 1, 1), padding='same', data_format=None)(img)
+    return KL.MaxPool3D(pool_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(img)
 
 
 def soft_open2D(img):
@@ -110,3 +111,28 @@ def soft_skel3D(img, iters):
         intersect = tf.math.multiply(skel, delta)
         skel += tf.nn.relu(delta-intersect)
     return skel
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    cube = np.zeros((10, 10, 10, 3))
+    cube[3:-3, 3:-3, 3:-3] = [0, 1, 0]
+    cube[:3]        = [1, 0, 0]
+    cube[-3:]       = [1, 0, 0]
+    cube[:, :3]     = [1, 0, 0]
+    cube[:, -3:]    = [1, 0, 0]
+    cube[:, :, :3]  = [1, 0, 0]
+    cube[:, :, -3:] = [1, 0, 0]
+    cube_tensor = tf.expand_dims(tf.convert_to_tensor(cube), 0)
+    print("tensor shape", cube_tensor.shape)
+
+    l, c = 2, 3
+    n = 4
+    plt.subplot(l, c, 1)
+    plt.imshow(cube[n])
+    plt.subplot(l, c, 2)
+    plt.imshow(soft_erode3D(cube_tensor).numpy()[0,n])
+    plt.show()
+
